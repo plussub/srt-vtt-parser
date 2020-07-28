0dcdaea05d5ced08f1971c0bfbee0b42fbc51903
stefanbreitenstein <stefan.breitenstein@tt-s.com>
2020-07-29 00:47:37 +0200

    introduce buggy state machine

--- src/srtParser.ts
+++ src/srtParser.ts
@@ -1,65 +1,73 @@
 import { ParsedResult, Entry } from './types';
-import { createMachine, interpret } from 'xstate';
 
-const machine = {
-  result: [] as any[],
-  current: {} as any,
-  transistions: {
-    start(tokens: string[], pos: number) {
-      machine.transistions.id(tokens, pos);
-    },
-    // @ts-ignore
-    id(tokens: string[], pos: number) {
-      if (tokens.length === pos) {
-        return machine.transistions.finish();
-      }
-      if (isBlank(tokens[pos])) {
-        return machine.transistions.id(tokens, ++pos);
-      }
+class SrtMachine{
+  result: Entry[] = [];
+  current: Partial<Entry> = {};
 
-      const idDoesNotExists = tokens[pos].includes('-->');
-      machine.current.id = idDoesNotExists ? '' : tokens[pos];
-      return machine.transistions.timeLine(tokens, idDoesNotExists ? pos : ++pos);
-    },
-    // @ts-ignore
-    timeLine(tokens: string[], pos: number) {
-      const timeLine = tokens[pos];
-      const [from, to] = timeLine.split('-->');
-      machine.current.from = timestampToMillisecond(from);
-      machine.current.to = timestampToMillisecond(to);
-      return machine.transistions.text(tokens, ++pos);
-    },
-    // @ts-ignore
-    text(tokens: string[], pos: number) {
-      if (tokens.length === pos) {
-        return machine.transistions.finish();
-      }
-      if (isBlank(tokens[pos])) {
-        return machine.transistions.finEntry(tokens, pos);
-      }
-      machine.current.text = tokens[pos];
-      return machine.transistions.multiLineText(tokens, ++pos);
-    },
-    // @ts-ignore
-    multiLineText(tokens: string[], pos: number) {
-      if (tokens.length === pos) {
-        return machine.transistions.finish();
-      }
-      if (isBlank(tokens[pos])) {
-        return machine.transistions.finEntry(tokens, pos);
-      }
-      machine.current.text = `${machine.current.text}\n${tokens[pos]}`;
-      return machine.transistions.multiLineText(tokens, ++pos);
-    },
-    // @ts-ignore
-    finEntry(tokens: string[], pos: number) {
-      machine.result.push({ ...machine.current });
-      machine.current = {};
-      return machine.transistions.id(tokens, ++pos);
-    },
-    finish() {}
+  constructor() {
   }
-};
+  start(raw: string): Entry[] {
+    this.id(raw.split(/\n/), 0);
+    return this.result;
+  }
+
+  id(tokens: string[], pos: number) {
+    if (tokens.length <= pos) {
+      this.finish();
+      return;
+    }
+    if (isBlank(tokens[pos])) {
+      this.id(tokens, ++pos);
+      return;
+    }
+
+    const idDoesNotExists = tokens[pos].includes('-->');
+    this.current.id = idDoesNotExists ? '' : tokens[pos];
+    this.timeLine(tokens, idDoesNotExists ? pos : ++pos);
+  }
+
+  timeLine(tokens: string[], pos: number) {
+    const timeLine = tokens[pos];
+    const [from, to] = timeLine.split('-->');
+    this.current.from = timestampToMillisecond(from);
+    this.current.to = timestampToMillisecond(to);
+    this.text(tokens, ++pos);
+  }
+
+  text(tokens: string[], pos: number) {
+    if (tokens.length <= pos) {
+      this.finish();
+      return;
+    }
+    if (isBlank(tokens[pos])) {
+      this.finEntry(tokens, pos);
+      return;
+    }
+    this.current.text = tokens[pos];
+    this.multiLineText(tokens, ++pos);
+  }
+
+  multiLineText(tokens: string[], pos: number) {
+    if (tokens.length <= pos) {
+      this.finish();
+      return;
+    }
+    if (isBlank(tokens[pos])) {
+      this.finEntry(tokens, pos);
+      return;
+    }
+    this.current.text = `${this.current.text}\n${tokens[pos]}`;
+    this.multiLineText(tokens, ++pos);
+  }
+
+  finEntry(tokens: string[], pos: number) {
+    // @ts-ignore
+    this.result.push({ ...this.current });
+    this.current = {};
+    this.id(tokens, ++pos);
+  }
+  finish() {}
+}
 const isBlank = (str: string) => !str || /^\s*$/.test(str);
 
 const timestampToMillisecond = (value: string) => {
@@ -68,8 +76,7 @@ const timestampToMillisecond = (value: string) => {
 };
 
 export const srtParser = (raw: string): ParsedResult => {
-  machine.transistions.start(raw.split(/\n/), 0);
   return {
-    entries: machine.result
+    entries: new SrtMachine().start(raw)
   };
 };
